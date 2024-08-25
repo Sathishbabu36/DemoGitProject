9b51b4d6352b90bc70d510aada87ce5370823ada
Sathishbabu36 <sathish03ece@gmail.com>
2024-08-25 19:35:01 +0530

    project intitated

---
+++ src/test/java/com/example/dgp/DemoGitProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dgp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
